e35d399d70eaf4542b38b4695cd61715a060ce1c
donnymboma01 <donnymboma01@gmail.com>
2023-02-26 23:31:24 +0100

    feat: resolver added

--- src/app/components/userdetail/userdetail.component.ts
+++ src/app/components/userdetail/userdetail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { tap } from 'rxjs/operators';
 import { Response } from 'src/app/models/response.interface';
+import { User } from 'src/app/models/user.interface';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -12,7 +13,8 @@ import { UserService } from 'src/app/services/user.service';
 export class UserdetailComponent implements OnInit {
   public response: Response;
   public mode: 'edit' | 'locked' = 'locked';
-  public buttonText: string = 'Edit';
+  public buttonText: 'Save Changes' | 'Edit' = 'Edit';
+  public user : User;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -20,19 +22,26 @@ export class UserdetailComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
-      this.userService
-        .getUser(params.get('uuid')!)
-        .pipe(
-          tap((response) => (this.response = response)),
-          tap((items) => console.log(items))
-        )
-        .subscribe();
-    });
+    // this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
+    //   this.userService
+    //     .getUser(params.get('uuid')!)
+    //     .pipe(
+    //       tap((response) => (this.response = response)),
+    //       tap((items) => console.log(items))
+    //     )
+    //     .subscribe();
+    // });
+    this.user = (<User>(this.activatedRoute.snapshot.data['resolvedResponse'].results[0]));
+    console.log(this.user);
   }
 
   public changeMode(mode?: 'edit' | 'locked'): void {
     console.log(mode);
     this.mode = this.mode === 'locked' ? 'edit' : 'locked';
+    this.buttonText = this.buttonText === 'Edit' ? 'Save Changes' : 'Edit';
+
+    if(mode === 'edit'){
+      console.log('Updated user on the backend');
+    }
   }
 }
